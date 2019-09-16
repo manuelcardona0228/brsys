d6db39490eeef727b41688ff00d077b57503923f
Manuel <manuel.cardonam@autonoma.edu.co>
2019-09-16 11:44:29 -0500

    Se realizaron mejoras a vistas

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -73,7 +73,7 @@
                                         <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
                                         <label class="form-check-label" for="remember">
-                                            {{ ('Remember Me') }}
+                                            {{ ('Recordarme') }}
                                         </label>
                                     </div>
                                 </div>
